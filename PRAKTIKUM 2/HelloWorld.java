c912f71dc728b880d9f28cc41f2898d0d6aea951
akuyudha <yudhabmc38v2@gmail.com>
2021-09-27 16:23:19 +0700

    HelloWorld.java

---
+++ PRAKTIKUM 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+
+	public static void main(String[] args) {
+		System.out.println("Nama : Elang Andhika Yudha Prawira");
+		System.out.println("NIM  : 6706213073");
+		System.out.println("Klas : 45-03");
+	}
+}
